4fa86396b2dc949dfbba6ab0cec5fa6b133e5023
SRIKARTHATI <srikar.velsol@gmail.com>
2019-07-10 19:19:26 +0530

    changes

---
+++ VelfoodsApi/Controllers/ItemcategoryController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VelfoodsApi.Models;
+
+namespace VelfoodsApi.Controllers
+{
+    public class ItemcategoryController : ApiController
+    {
+        velfoodsEntities1 entity = new velfoodsEntities1();
+        Responce re = new Responce();
+        [HttpPost]
+        [Route("getcategories")]
+        public Responce getvalues(vel_restro_itemcategory itemc)
+        {
+            var res = (from c in entity.vel_restro_itemcategory
+                       where c.restaurent_id == itemc.restaurent_id
+                       select new
+                       {
+                           c.item_name,
+                           c.item_description,
+                           c.item_active_from,
+                           c.item_status,
+                           c.item_reporting_name,
+                           c.restaurent_id
+                       });
+            re.Data = res;
+            re.message = "Data sucess";
+            re.code = 200;
+            return re;
+        }
+
+        [HttpPost]
+        [Route("itemcinsert")]
+        public IHttpActionResult insert(vel_restro_itemcategory itemc)
+        {
+            Boolean b = new itemcategoryclass().adding(itemc);
+            if (b)
+            {
+                entity.vel_restro_itemcategory.Add(itemc);
+                entity.SaveChanges();
+                re.code = 200;
+                re.message = "Data inserted successfully";
+                return Content(HttpStatusCode.OK, re);
+            }
+            else
+            {
+                re.code = 100;
+                re.message = "insert fail check the values";
+                return Content(HttpStatusCode.OK, re);
+            }
+        }
+
+        [HttpPost]
+        [Route("itemcatupdate")]
+        public IHttpActionResult update(vel_restro_itemcategory itcate)
+        {
+            Boolean b = new itemcategoryclass().update(itcate);
+            if (b)
+            {
+                re.code = 200;
+                re.message = "Data updated successfully";
+                return Content(HttpStatusCode.OK, re);
+            }
+            else
+            {
+                re.code = 100;
+                re.message = "update fail check the values";
+                return Content(HttpStatusCode.OK, re);
+            }
+        }
+    }
+}
---
+++ VelfoodsApi/Controllers/itemnameController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VelfoodsApi.Models;
+
+namespace VelfoodsApi.Controllers
+{
+    public class itemnameController : ApiController
+    {
+
+        velfoodsEntities1 entity = new velfoodsEntities1();
+        Responce re = new Responce();
+
+
+        [HttpPost]
+        [Route("getitemnames")]
+        public Responce get(vel_restro_itemname iname)
+        {
+            var cc = (from c in entity.vel_restro_itemname
+                      where c.restaurent_id == iname.restaurent_id
+                      select new
+                      {
+                          c.itemname_id,
+                          c.itemname_item_name,
+                          c.itemname_description,
+                          c.itemname_reportingname,
+                          c.itemname_active_from,
+                          c.itemname_status,
+                          c.item_dinein_amount,
+                          c.item_dinein_tax,
+                          c.item_takeaway_amount,
+                          c.item_takeaway_tax,
+                          c.item_homedelivary_amount,
+                          c.item_homedelivary_tax,
+                          c.item_homedelivery_deliverycharges
+                      });
+            re.Data = cc;
+            re.message = "Data success";
+            re.code = 200;
+            return re;
+        }
+
+        [HttpPost]
+        [Route("itemnameinsert")]
+        public IHttpActionResult insert(vel_restro_itemname name)
+        {
+            Boolean b = new itemnameClass().adding(name);
+            if (b)
+            {
+                entity.vel_restro_itemname.Add(name);
+                entity.SaveChanges();
+                re.code = 200;
+                re.message = "Data inserted Successfully";
+                return Content(HttpStatusCode.OK, re);
+            }
+            else
+            {
+                re.code = 100;
+                re.message = "inserted fail please check the values ";
+                return Content(HttpStatusCode.OK, re);
+            }
+        }
+
+        [HttpPost]
+        [Route("itemnameupdate")]
+        public IHttpActionResult update(vel_restro_itemname name)
+        {
+            Boolean b = new itemnameClass().update(name);
+            if (b)
+            {
+                re.code = 200;
+                re.message = "Data updated Successfully";
+                return Content(HttpStatusCode.OK, re);
+            }
+            else
+            {
+                re.code = 100;
+                re.message = "updated fail please check the values ";
+                return Content(HttpStatusCode.OK, re);
+            }
+        }
+
+    }
+}
