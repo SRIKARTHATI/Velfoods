1cedf9cc54edde37d3a032f25eb32a00625556d4
GuduruUmesh <35020243+GuduruUmesh@users.noreply.github.com>
2019-07-11 17:01:39 +0530

    changes

---
+++ VelfoodsApi/Controllers/OffersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Web.Http;
+using VelfoodsApi.Models;
+
+namespace VelfoodsApi.Controllers
+{
+    public class OffersController : ApiController
+    {
+        velfoodsEntities1 entity = new velfoodsEntities1();
+        Responce re = new Responce();
+        [HttpPost]
+        [Route("OfferAdding")]
+        public IHttpActionResult addOffer(vel_restro_offers offers)
+        {
+            Boolean b = new Offers().adding(offers);
+            if (b)
+            {
+                entity.vel_restro_offers.Add(offers);
+                entity.SaveChanges();
+                re.code = 200;
+                re.message = "Offer added Successfully";
+                return Content(HttpStatusCode.OK, re);
+            }
+            else
+            {
+                re.code = 100;
+                re.message = "Failed to insert Please check the values..!";
+                return Content(HttpStatusCode.OK, re);
+            }
+        }
+    }
+}
---
+++ VelfoodsApi/Models/Offers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VelfoodsApi.Models
+{
+    public class Offers
+    {
+        String promocode,promoname;
+        int count, empid;
+        public Boolean adding(vel_restro_offers offers)
+        {
+            List<vel_restro_offers> list = new List<vel_restro_offers>();
+            using (velfoodsEntities1 en = new velfoodsEntities1())
+            {
+                list = en.vel_restro_offers.OrderBy(a => a.offers_id).ToList();
+                int cc = list.Count;
+                for (int i = 0; i < cc; i++)
+                {
+                    promocode = list[i].promo_code;
+                    promoname = list[i].promo_code_name;
+                    if (offers.promo_code.Equals(promocode) || offers.promo_code_name.Equals(promoname))
+                    {
+                        count = 1;
+                        break;
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+                }
+                if (count == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
