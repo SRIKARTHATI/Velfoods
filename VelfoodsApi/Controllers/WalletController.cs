5eeca9acaef1c37db882951e6dc298385c2ca39e
Aneesha <aneeshap.velsol@gmail.com>
2019-08-09 11:39:24 +0530

    changes

---
+++ VelfoodsApi/Controllers/WalletController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VelfoodsApi.Models;
+
+namespace VelfoodsApi.Controllers
+{
+    public class WalletController : ApiController
+    {
+        velfoodsEntities2 ve = new velfoodsEntities2();
+        Responce re = new Responce();
+        [HttpPost]
+        [Route("getwallets")]
+        public Responce getwallets(vel_restro_wallet tbl)
+        {
+            var ee = (from s in ve.vel_restro_wallet
+                      where s.restaurent_id == tbl.restaurent_id
+                      select new
+                      {
+                          s.wallet_id,
+                          s.wallet_code,
+                          s.wallet_name,
+                          s.wallet_reporting_name,
+                          s.status,
+                      });
+
+            re.Data = ee;
+            re.message = "getting details successfully";
+            re.code = 200;
+            return re;
+        }
+
+        [HttpPost]
+        [Route("walletadding")]
+        public IHttpActionResult AddingWallet(vel_restro_wallet tbl)
+        {
+            Boolean b = new WalletClass().walletadd(tbl);
+            if (b)
+            {
+                tbl.BACKGROUND_COLOR = "Green";
+                ve.vel_restro_tabledefination.Add(tbl);
+                ve.SaveChanges();
+                re.code = 200;
+                re.message = "data inserted succesfully";
+                return Content(HttpStatusCode.OK, re);
+            }
+            else
+            {
+                re.code = 100;
+                re.message = "failed to insert data";
+                return Content(HttpStatusCode.OK, re);
+            }
+        }
+
+        [HttpPost]
+        [Route("walletupdate")]
+        public IHttpActionResult Updatewallet(vel_restro_wallet tbl)
+        {
+            Boolean b = new WalletClass().walletupdate(tbl);
+            if (b)
+            {
+                re.code = 200;
+                re.message = "data Updated succesfully";
+                return Content(HttpStatusCode.OK, re);
+            }
+            else
+            {
+                re.code = 100;
+                re.message = "failed to update data";
+                return Content(HttpStatusCode.OK, re);
+            }
+        }
+    }
+}
---
+++ VelfoodsApi/Models/WalletClass.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VelfoodsApi.Models
+{
+    public class WalletClass
+    {
+        public int wallet_code;
+        public string wallet_name;
+        public string wallet_reporting_name;
+        public string status;
+        public int restaurent_id;
+        public int count;
+        public Boolean walletadd(vel_restro_wallet veltbl)
+        {
+            List<vel_restro_wallet> list = new List<vel_restro_wallet>();
+            using (velfoodsEntities2 en = new velfoodsEntities2())
+            {
+                list = en.vel_restro_wallet.OrderBy(a => a.wallet_id).ToList();
+                int c = list.Count;
+                veltbl.restaurent_id = 1;
+                for (int i = 0; i < c; i++)
+                {
+                    wallet_name = list[i].wallet_name;
+                    wallet_reporting_name = list[i].wallet_reporting_name;
+                    wallet_code = list[i].wallet_code;
+                    status = list[i].status;
+                    restaurent_id = list[i].restaurent_id;
+                    if (veltbl.wallet_name.Equals(wallet_name) && veltbl.wallet_reporting_name.Equals(wallet_reporting_name) && veltbl.wallet_code.Equals(wallet_code) && veltbl.status.Equals(status) )
+                    {
+                        count = 1;
+                        break;
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+                }
+                if (count == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public Boolean walletupdate(vel_restro_wallet tbl)
+        {
+            List<vel_restro_wallet> list = new List<vel_restro_wallet>();
+
+            using (velfoodsEntities2 en = new velfoodsEntities2())
+            {
+                list = en.vel_restro_wallet.OrderBy(a => a.wallet_id).ToList();
+                int c = list.Count;
+                for (int i = 0; i < c; i++)
+                {
+                    wallet_name = list[i].table_name;
+                    wallet_code = list[i].wallet_code;
+                    if (wallet_code.Equals(wallet_code))
+                    {
+                        count = 1;
+                        break;
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+                }
+                if (count == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    using (velfoodsEntities2 entit = new velfoodsEntities2())
+                    {
+                        vel_restro_wallet vp = (from s in entit.vel_restro_wallet
+                                                         where s.wallet_id == tbl.wallet_id
+                                                         where s.restaurent_id == tbl.restaurent_id
+                                                         select s).FirstOrDefault();
+                        vp.wallet_name = tbl.wallet_name;
+                        vp.wallet_reporting_name = tbl.wallet_reporting_name;
+                        vp.status = tbl.status;
+                        entit.SaveChanges();
+                    }
+                    return true;
+                }
+            }
+        }
+    }
+}
