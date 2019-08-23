4055392e71fc18ff097c526cd53a11a65b974a23
Aneesha <aneeshap.velsol@gmail.com>
2019-08-23 18:46:59 +0530

    changes

--- VelfoodsApi/Controllers/loginController.cs
+++ VelfoodsApi/Controllers/loginController.cs
@@ -15,6 +15,7 @@ namespace VelfoodsApi.Controllers
         Responce re = new Responce();
         string uname, pwd;
         Boolean cou;
+        int id;
         public static int resid;
 
         [HttpPost]
@@ -68,13 +69,15 @@ namespace VelfoodsApi.Controllers
                     {
                         uname = mangers[i].username;
                         pwd = mangers[i].password;
-                        resid = mangers[i].restaurent_id;
+                        id = mangers[i].restaurent_id;
                         if (uname.Equals(aa.username) && pwd.Equals(aa.password))
                         {
+                            resid = id;
+                            re.resid = resid;
                             re.code = 200;
                             re.message = "Manger Login successfully";
                             cou = true;
-                            resid = mangers[i].restaurent_id;
+                          
                         }
                         else
                         {
@@ -88,13 +91,15 @@ namespace VelfoodsApi.Controllers
                     {
                         uname = casier[i].Username;
                         pwd = casier[i].password;
-                        resid = casier[i].restaurent_id;
+                        id = casier[i].restaurent_id;
                         if (uname.Equals(aa.username) && pwd.Equals(aa.password))
                         {
+                            resid = id;
+                            re.resid = resid;
                             re.code = 200;
                             re.message = "casier Login successfully";
                             cou = true;
-                            resid = casier[i].restaurent_id;
+                         
                         }
                         else
                         {
@@ -121,6 +126,52 @@ namespace VelfoodsApi.Controllers
                 return Content(HttpStatusCode.OK, re);
             }
 
+        }
+
+        [HttpPost]
+        [Route("sucess")]
+        public Responce success(admin a)
+        {
+            var admin = (from c in entity.vel_restro_Admin
+                         where c.admin_status == "Active"
+                         where c.admin_username == a.username
+                         where c.admin_password == a.password
+                         select c).ToList();
+            int count = admin.Count;
+            //manger
+            var mangers = (from cc in entity.vel_restro_manger
+                           where cc.manger_status == "Active"
+                           where cc.username == a.username
+                           where cc.password ==a.password
+                           select cc).ToList();
+            int count1 = mangers.Count;
+            //casier
+            var casier = (from cash in entity.vel_restro_empregistration
+                          join cash1 in entity.vel_restro_empdepartment on cash.empdepartement_id equals cash1.empdepartement_id
+                          where cash1.empdepartement_name == "Cashier"
+                          where cash.empregistration_status == "Active"
+                          where cash.Username ==a.username
+                          where cash.password ==a.password
+                          select cash).ToList();
+            int count2 = casier.Count;
+            if(count >0 || count1 >0 ||count2 > 0)
+            {
+                if (count > 0)
+                {
+                    
+                }
+                if(count >1)
+                {
+                    resid = mangers[0].restaurent_id;
+                }
+                if (count > 2)
+                {
+                    resid = casier[0].restaurent_id;
+                }
+            }
+            re.code = 200;
+            re.message = "Data success";
+            return re;
         }
     }
 }
